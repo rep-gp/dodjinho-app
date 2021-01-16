553f4b695e6959fdbd33fed6aae514aff629aba2
Guiqft <g00_deoliveira@outlook.com>
2021-01-16 20:54:40 -0300

    [BUTTON_COMPONENT]: Commit without .nuxt

---
+++ pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-container">
+        Home {{ valor }}
+        <flat-button title="Jogar"/>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    layout: 'solid',
+    data() {
+        return {
+            valor: 'aaaaa'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.home-container {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+}
+
+</style>
