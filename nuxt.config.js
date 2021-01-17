3fa387479cca68e36c8b0a85eebf2bba990a3f25
Bruno Mengaldo <39499843+brnom@users.noreply.github.com>
2021-01-17 01:11:51 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
-        title: 'Dodjinho',
+        title: 'E-duca',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
