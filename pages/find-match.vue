f61d29dbf472bfcc66603a2f2f0ee93e25653518
Henrique Cunha <henriquecunhawd@gmail.com>
2021-01-16 23:32:43 -0300

    [FIND_MATCH_PAGE]: Add subjects and game modes

--- pages/find-match.vue
+++ pages/find-match.vue
@@ -6,17 +6,47 @@
                 Solo
             </div>
             <div class="options">
-                <icon-button />
+                <icon-button icon="single-user">
+                    1x1
+                </icon-button>
+                <icon-button icon="multiple-users">
+                    3x3
+                </icon-button>
+                <icon-button icon="pen">
+                    Treino
+                </icon-button>
             </div>
         </div>
         <div class="group-matching">
             <div class="title">
                 Grupo
             </div>
+            <div class="options">
+                <icon-button icon="single-user">
+                    1x1
+                </icon-button>
+                <icon-button icon="multiple-users">
+                    3x3
+                </icon-button>
+                <icon-button icon="shield">
+                    Grupo
+                </icon-button>
+            </div>
         </div>
+
         <div class="subjects">
             <div class="title">
                 Assuntos
+            </div>
+            <div class="options">
+                <icon-button
+                    v-for="subject in subjectList"
+                    :key="subject.label"
+                    :icon="subject.icon"
+                    big-icon
+                >
+                    {{ subject.label }}
+                </icon-button>
             </div>
         </div>
     </div>
@@ -26,14 +56,28 @@
 import Vue from 'vue'
 export default Vue.extend({
     
-    layout: 'solid'
+    layout: 'solid',
+    computed: {
+        subjectList () {
+            return [
+                { icon: 'technology', label: 'Tecnologia' },
+                { icon: 'geography', label: 'Geografia' },
+                { icon: 'history', label: 'História' },
+                { icon: 'science', label: 'Ciência' },
+                { icon: 'lightbulb', label: 'Cultura' },
+                { icon: 'chemistry', label: 'Química' }
+            ]
+        }
+    }
 
 })
 </script>
 
 <style lang="scss" scoped>
 .find-match {
+    width: 100%;
     padding: 20px;
+    box-sizing: border-box;
     & > .header {
         height: 7.5vh;
     }
@@ -41,6 +85,22 @@ export default Vue.extend({
         font-weight: bold;
         color: #ffffff;
         font-size: 14pt;
+        margin-top: 15px;
+    }
+    & > .solo-matching, & > .group-matching {
+        .options {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 10px;
+        }
+    }
+    & > .subjects {
+        margin-top: 40px;
+        & > .options {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 20px;
+        }
     }
 }
 </style>
